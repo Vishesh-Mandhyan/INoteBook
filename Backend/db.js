3a8c268b7e730d534c700c22f3d70af8f1a1bab1
Vishesh Mandhyan <vmandhyan27@gmail.com>
2021-12-01 18:07:44 +0530

    database issue fixed

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
-// const url = process.env.MONGO_URI
+const url = process.env.MONGO_URI
 // connectiing to the database 
 const connectToMongo = async() => {
-    await mongoose.connect( "mongodb+srv://vishesh:methehero@cluster0.isgwx.mongodb.net/inotebook")
+    await mongoose.connect(url)
     console.log("connected to mongo successfully");
 };
 module.exports = connectToMongo;
